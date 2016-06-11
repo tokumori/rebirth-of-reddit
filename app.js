efad479e258b2d05616bc0ce46d6a41888fcd940
Bryce <bsaito88@gmail.com>
2016-06-11 13:38:37 -1000

    Changed most elements from divs to specific tags, and created a div to contain the posts.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var mainPage = document.getElementById('main');
 
 $.ajax({
   method: 'GET',
-  url: 'http://www.reddit.com/r/funny.json',
+  url: 'http://www.reddit.com/r/newsokur.json',
   dataType: 'json'
 })
 .done(function (data) {
@@ -18,27 +18,50 @@ $.ajax({
 
 function processResponse (response) {
   childArr = response.data.children;
-  childArr.forEach(createTitle);
+  childArr.forEach(createPage);
 }
 
-function createTitle (elem, ind, arr) {
+function createPage (elem, ind, arr) {
+  var post = document.createElement('div');
+  post.className = 'post';
+  mainPage.appendChild(post);
+
+  var thumbnailImg = document.createElement('IMG');
+  if (elem.data.thumbnail === 'self') {
+    thumbnailImg.src = 'http://a.thumbs.redditmedia.com/ExQ61Q54Z-aAuJpkFNcC0viWh-2iQcEc9HrocEZcxw8.jpg';
+  } else {
+    thumbnailImg.src = elem.data.thumbnail;
+  }
+  thumbnailImg.className = 'img';
+  var thumbnailLink = document.createElement('a');
+  thumbnailLink.href = elem.data.url;
+  thumbnailLink.className = 'thumbnail';
+  thumbnailLink.appendChild(thumbnailImg);
+  post.appendChild(thumbnailLink);
+
+  var info = document.createElement('div');
+  info.className = 'info';
+  post.appendChild(info);
+
   var title = document.createElement('h3');
   title.className = 'title';
   title.innerHTML = '<a href=' + elem.data.url + '>'+ elem.data.title + '</a>';
-  mainPage.appendChild(title);
+  info.appendChild(title);
 
-  var thumbnail = document.createElement('div');
-  thumbnail.className = 'thumbnail';
-  thumbnail.innerHTML = '<a href=' + elem.data.url + '><img src=' + elem.data.thumbnail + '></a>';
-  mainPage.appendChild(thumbnail);
+  var date = document.createElement('div');
+  date.className = 'date';
+  date.innerHTML = elem.data.created;
+  info.appendChild(date);
 
-  var author = document.createElement('div');
+  var author = document.createElement('a');
   author.className = 'author';
-  author.innerHTML = '<a href=http://www.reddit.com/user/' + elem.data.author + '>' + elem.data.author + '</a>';
-  mainPage.appendChild(author);
+  author.href = 'http://www.reddit.com/user/' + elem.data.author;
+  author.innerHTML = elem.data.author;
+  info.appendChild(author);
 
-  var comments = document.createElement('div');
+  var comments = document.createElement('a');
   comments.className = 'comments';
-  comments.innerHTML = '<a href=http://www.reddit.com' + elem.data.permalink+ '>' + elem.data.num_comments + ' comments' + '</a>';
-  mainPage.appendChild(comments);
+  comments.href = 'http://www.reddit.com' + elem.data.permalink;
+  comments.innerHTML = elem.data.num_comments + ' comments';
+  info.appendChild(comments);
 }
