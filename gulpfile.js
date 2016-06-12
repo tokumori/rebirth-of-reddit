d14d5a02c895215dda14ae0f316fe50285bfa312
Bryce <bsaito88@gmail.com>
2016-06-11 14:10:49 -1000

    Added upkeep task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 
+gulp.task('upkeep', function () {
+  gulp.watch('app.js', function () {
+    console.log('The file changed');
+  });
+});
+
 gulp.task('default', function () {
   console.log('halp me!');
 });
