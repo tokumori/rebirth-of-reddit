ade37e0da53b1342f73d497b5604d3c728bd7e19
Bryce <bsaito88@gmail.com>
2016-06-11 14:52:45 -1000

    Got Sass to work and compile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+  gulp
+  .src('scss/styles.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'));
+});
 
 gulp.task('upkeep', function () {
   gulp.watch('app.js', function () {
